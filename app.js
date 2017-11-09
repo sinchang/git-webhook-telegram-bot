46bccc0fe4150db3aea82cfafcb244404ef7f8a0
sinchang <sinchangwen@gmail.com>
2017-11-09 17:52:28 +0800

    add firebase

--- app.js
+++ app.js
@@ -1,36 +1,56 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const TelegramBot = require('node-telegram-bot-api')
+const firebase = require('firebase')
+const uniqueString = require('unique-string')
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '412096935:AAGEPdzS-oQh6dCpaJ3qw_cyrYiu2OGujvw'
+const url = 'https://gitlab-telegram-bot.herokuapp.com'
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true})
 const app = express()
 const PORT = process.env.PORT || 3000
 
+firebase.initializeApp({
+  apiKey: 'AIzaSyA0F3mHAqEMsDDKX4-rodIZ_sO3bdsp8lk',
+  databaseURL: 'https://gitlab-telegram-bot.firebaseio.com'
+})
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message')
+bot.onText(/\/add/, (msg) => {
+  const userId = msg.chat.id
+  const repoName = msg.message.text.substring(5)
+  const repoId = uniqueString()
+  const webhookUrl = `${url}/webhook/${repoId}`
+  firebase.database().ref(`repos/${repoId}`).set({
+    webhookUrl,
+    repoName,
+    userId: userId,
+    username: msg.chat.username
+  }).then(() => {
+    console.log(repoName)
+    bot.sendMessage(userId, `add succeed!!! repository name is ${repoName} and webhook url is ${webhookUrl}`)
+  })
 })
 
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.post('/telegram', (req, res) => {
-  cosole.log('22')
-})
-
-app.post('/webhook1', (req, res) => {
+app.post('/webhook/:id', (req, res) => {
   const body = req.body
+  const repoId = req.params.id
   const message = `[${body.project.path_with_namespace}]: ${body.event_name} by ${body.user_name}`
-  console.log(message)
-  res.send(message)
+  firebase.database().ref(`repos/${repoId}`).once('value')
+  .then(snapshot => {
+    const { userId } = snapshot.val()
+    console.log(userId)
+    bot.sendMessage(userId, message)
+    res.send('succeed')
+  })
 })
 
 app.listen(PORT, () => {
