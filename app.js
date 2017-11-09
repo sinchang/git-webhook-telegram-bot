49b5132fe41827f51a813ddded74fb10b0c4d17d
sinchang <sinchangwen@gmail.com>
2017-11-09 20:42:10 +0800

    update variable

--- app.js
+++ app.js
@@ -4,23 +4,36 @@ const TelegramBot = require('node-telegram-bot-api')
 const firebase = require('firebase')
 const uniqueString = require('unique-string')
 
+require('dotenv').config()
+
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '412096935:AAGEPdzS-oQh6dCpaJ3qw_cyrYiu2OGujvw'
-const url = 'https://gitlab-telegram-bot.herokuapp.com'
+const token = process.env.BOT_TOKEN
+const url = process.env.WEBHOOK_URL
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true})
+const bot = new TelegramBot(token, {
+  polling: true
+})
 const app = express()
 const PORT = process.env.PORT || 3000
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyDmqhQ6Kcq6KdE7R4B9gZebPsiwUbGNIgs',
-  databaseURL: 'https://gitlab-telegram-bot.firebaseio.com'
+  apiKey: process.env.FIREBASE_KEY,
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 })
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
-bot.onText(/\/add/, (msg) => {
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, `
+    - command _/add {name}_ -- add new repository
+    - command _/search {repository id}_ -- search repository info
+  `)
+})
+
+bot.onText(/\/add/, msg => {
   const userId = msg.chat.id
   const repoName = msg.text.substring(5)
   const repoId = uniqueString()
@@ -31,9 +44,33 @@ bot.onText(/\/add/, (msg) => {
     userId: userId,
     username: msg.chat.username
   }).then(() => {
-    console.log(repoName)
-    bot.sendMessage(userId, `add succeed!!! repository name is ${repoName} and webhook url is ${webhookUrl}`)
+    bot.sendMessage(userId, `congrats!!!
+      - repository name is ${repoName}
+      - unique repository id is ${repoId} (important)
+      - webhook url is ${webhookUrl}`)
+  }).catch(error => {
+    bot.sendMessage(error.message)
   })
+})
+
+bot.onText(/\/search/, msg => {
+  const userId = msg.chat.id
+  const repoId = msg.text.substring(8)
+  firebase.database().ref(`repos/${repoId}`).once('value')
+    .then(snapshot => {
+      const {
+        webhookUrl,
+        username,
+        userId,
+        repoName
+      } = snapshot
+      bot.sendMessage(userId, `congrats!!!
+      - repoName url is ${repoName}
+      - username is ${username}
+      - webhook url is ${webhookUrl}`)
+    }).catch(error => {
+      bot.sendMessage(error.message)
+    })
 })
 
 app.get('/', (req, res) => {
@@ -45,12 +82,19 @@ app.post('/webhook/:id', (req, res) => {
   const repoId = req.params.id
   const message = `[${body.project.path_with_namespace}]: ${body.event_name} by ${body.user_name}`
   firebase.database().ref(`repos/${repoId}`).once('value')
-  .then(snapshot => {
-    const { userId } = snapshot.val()
-    console.log(userId)
-    bot.sendMessage(userId, message)
-    res.send('succeed')
-  })
+    .then(snapshot => {
+      const {
+        userId
+      } = snapshot.val()
+      console.log(userId)
+      bot.sendMessage(userId, message)
+      res.send('succeed')
+    })
+})
+
+app.post('/webhook1', (req, res) => {
+  console.log(req.body)
+  res.send('succeed')
 })
 
 app.listen(PORT, () => {
